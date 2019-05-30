414ffe7bda4601e9e43e6f1d6f8bc38aa60ffbe6
mistray <542035625@qq.com>
2019-05-30 14:04:30 +0800

    提交序列化工具

---
+++ src/main/java/com/mistray/im/serialize/Serializer.java
@@ -0,0 +1,28 @@
+package com.mistray.im.serialize;
+
+
+import com.mistray.im.serialize.impl.JSONSerializer;
+
+/**
+ * @author james mu
+ * @date 18-12-3 下午3:24
+ */
+public interface Serializer {
+    Serializer DEFAULT = new JSONSerializer();
+
+    /**
+     * 序列化算法
+     * @return
+     */
+    byte getSerializerAlgorithm();
+
+    /**
+     * java 对象转换成二进制
+     */
+    byte[] serialize(Object object);
+
+    /**
+     * 二进制转换成 java 对象
+     */
+    <T> T deserialize(Class<T> clazz, byte[] bytes);
+}
